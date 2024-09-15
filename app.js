20ee64141745f2a902d21437a489e344945bfc32
Bipin Timilsena <bipintimilsen@gmail.com>
2024-09-15 19:46:50 +0530

    day 1 of react

---
+++ app.js
@@ -0,0 +1,41 @@
+const parent=React.createElement(
+    "div",
+    {id:"heading"},
+   [ React.createElement(
+        "div",
+        {id:"child"},
+       [React.createElement(
+            "h1",
+            {},
+            "I am an h1 Tag"
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "My name is sanjay kumar"
+        )
+
+       ]
+    ),
+    React.createElement(
+        "div",
+        {id:"child 2"},
+       [React.createElement(
+            "h1",
+            {},
+            "wow nice awesome"
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "we are waiting"
+        )
+
+       ]
+    )
+
+]
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent);
